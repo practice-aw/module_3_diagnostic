f9c8b12fbd52188f8ce5f37c8195d1f8ba96f537
Alec Wells <alec.wells47@gmail.com>
2019-10-14 10:40:56 -0600

    Make view more dynamic

--- spec/features/search_by_house_spec.rb
+++ spec/features/search_by_house_spec.rb
@@ -12,7 +12,7 @@ describe "user can search for characters" do
     # Then my path should be "/search" with "state=CO" in the parameters
     expect(current_path).to eq(search_path)
     # And I should see a message "7 Results"
-    expect(page).to have_content("21 Results")
+    expect(page).to have_content("21 Members in house Gryffindor")
     # And I should see a list of 7 the members of the house for Colorado
     # And I should see a name, role, party, and district for each member
     within(".characters") do
